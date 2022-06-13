ec834db959460702f64af49fae2fad0659acaefc
alexandria.gomez <alexandria.gomez@WATSONVILLE>
2022-06-13 15:34:13 -0700

    remove export of notification

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export * from './service-implementations/lf-repo-tree-service.js';
 export * from './helper-types/lf-repository-types.js';
 export * from './helper-types/lf-default-fields.service.js';
 export * from './helper-types/lf-tree-types.js';
-export * from './helper-types/lf-notification.js';
 export * from './helper-types/repository-api-ex.js';
