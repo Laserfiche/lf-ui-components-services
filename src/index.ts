9a8b311018f6d2c809e7257dd9d825e6d9736498
alexandria.gomez <alexandria.gomez@WATSONVILLE>
2022-08-26 12:12:42 -0400

    only have repoTreeNodeService

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './service-implementations/lf-fields.service.js';
-export * from './service-implementations/lf-repo-tree-service.js';
 export * from './service-implementations/lf-repo-tree-node-service.js';
 export * from './helper-types/lf-repository-types.js';
 export * from './helper-types/lf-default-fields.service.js';
