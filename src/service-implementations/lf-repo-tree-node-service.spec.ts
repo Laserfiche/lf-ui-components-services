04990ef6adb04bb03ee888e067dfb7ad1984a063
rfulton-lf <robert.fulton@laserfiche.com>
2023-02-03 16:29:16 -0800

    fixed typo

--- src/service-implementations/lf-repo-tree-node-service.spec.ts
+++ src/service-implementations/lf-repo-tree-node-service.spec.ts
@@ -281,7 +281,7 @@ describe('LfRepoTreeNodeService', () => {
     expect(createdNode).toEqual(expectedNode);
 
   });
-  it('should correctly format elecDocumentSize in b, KB, MB, GB, TB', () => {
+  it('should correctly format elecDocumentSize in B, KB, MB, GB, TB', () => {
     const expectedNode: LfRepoTreeNode = {
       name: 'DummyDocument',
       path: '\\DummyDocument',
